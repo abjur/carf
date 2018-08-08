8f47ef412ea22862ce0e5684f0b34da9d31c4f66
Bruno Daleffi <brunodaleffi@gmail.com>
2018-08-08 09:27:56 -0300

    comprot atualizado

--- R/comprot.R
+++ R/comprot.R
@@ -18,12 +18,12 @@
 #'
 #' @export
 get_comprot <- function(id) {
-
+  
   # Clean IDs
   id <- stringr::str_replace_all(id, "[^0-9]", "")
-
+  
   # Iterate over IDs
-  purrr::map_dfr(id, get_comprot_)
+  purrr::map(id, get_comprot_)
 }
 
 #' Get the comprot data for one lawsuit
@@ -31,28 +31,28 @@ get_comprot <- function(id) {
 #' @param id Character vector with lawsuit ID
 #' @return One row containing lawsuit's comprot data
 get_comprot_ <- purrr::possibly(function(id) {
-
+  
   # Fabricate a timestamp
   ts <- stats::runif(1, 0, 999) %>%
     round() %>%
     stringr::str_pad(3, "left", "0") %>%
     stringr::str_c(as.integer(lubridate::now()), .)
-
+  
   # Create URL for query
   base <- "https://comprot.fazenda.gov.br/comprotegov"
   u_query <- glue::glue(base, "/api/processo/{id}?_={ts}")
-
+  
   # Get comprot and convert from JSON
   comprot <- u_query %>%
     httr::GET(httr::config(ssl_verifypeer = FALSE)) %>%
     xml2::read_html() %>%
     rvest::html_text() %>%
     jsonlite::fromJSON()
-
+  
   # Separate lawsuit and events
   lwst <- comprot$processo
   movs <- comprot$movimentos
-
+  
   # Clean lawsuit row
   lwst <- lwst %>%
     dplyr::as_tibble() %>%
@@ -65,7 +65,7 @@ get_comprot_ <- purrr::possibly(function(id) {
     purrr::set_names(c(
       "id", "date", "document", "origin", "subject",
       "party", "cpf_cnpj", "status", "state"))
-
+  
   # Clean events table
   movs <- movs %>%
     dplyr::as_tibble() %>%
@@ -78,6 +78,6 @@ get_comprot_ <- purrr::possibly(function(id) {
     purrr::set_names(c(
       "date_event", "type", "number", "id_event", "id_origin",
       "name_origin", "id_destination", "name_destination"))
-
+  
   return(dplyr::mutate(lwst, events = list(movs)))
 },'deu ruim')
